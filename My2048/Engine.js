dfe3fd844a73086affe3b756b4769895f00d5623
Frederic Forjan <fforjan@free.fr>
2014-03-20 22:30:24 +1100

    Initial Import

---
+++ My2048/Engine.js
@@ -0,0 +1,34 @@
+﻿
+function Board ()  {
+    this.Content = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']]
+};
+
+Board.prototype.getCell = function()
+{
+    var x = Math.floor((Math.random() * 4));
+    var y = Math.floor((Math.random() * 4));
+    return [x, y];
+},
+
+Board.prototype.getCellValue= function(position)
+{
+    return this.Content[position[0]][position[1]];
+};
+
+Board.prototype.setCellValue = function (position, value) {
+    this.Content[position[0]][position[1]] = value;
+};
+
+Board.prototype.getEmptyCell = function () {
+    var cell = this.getCell();
+    while (this.getCellValue(cell) != '') {
+        cell = this.getCell();
+    }
+
+    return cell;
+};
+
+Board.prototype.fillBoard = function () {
+    var cell = this.getEmptyCell();
+    this.setCellValue(cell, Math.random() > 0.75 ? 4 : 2);
+};
