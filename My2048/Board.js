1b14e8f03a614ebd5084bacdd1b8acf3c943d087
Frederic Forjan <frederic.forjan@schneider-electric.com>
2014-03-21 14:26:04 +1100

    JSLINT Inside !

--- My2048/Board.js
+++ My2048/Board.js
@@ -1,38 +1,47 @@
 /**
  * Board constructor
  */
-function Board ()  {
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+
+function Board() {
+
+    "use strict";
     this.Content = [['', '', '', ''], ['', '', '', ''], ['', '', '', ''], ['', '', '', '']];
     this.LastInserted = [-1, -1];
 }
 
-Board.prototype.getCell = function()
-{
-    var x = Math.floor((Math.random() * 4));
-    var y = Math.floor((Math.random() * 4));
+Board.prototype.getCell = function () {
+    "use strict";
+    var x = Math.floor((Math.random() * 4)), y = Math.floor((Math.random() * 4));
     return [x, y];
-},
+};
 
-Board.prototype.getCellValue= function(position)
-{
+/**
+ Get the current cell value
+ @param position array for the coordinate
+ */
+Board.prototype.getCellValue = function (position) {
+    "use strict";
     return this.Content[position[0]][position[1]];
 };
 
-Board.prototype.clearCellValue= function(position)
-{
+Board.prototype.clearCellValue = function (position) {
+    "use strict";
     this.Content[position[0]][position[1]] = '';
 };
 
-Board.prototype.isCellEmpty= function(position)
-{
+Board.prototype.isCellEmpty = function (position) {
+    "use strict";
     return this.Content[position[0]][position[1]] === '';
 };
 
 Board.prototype.setCellValue = function (position, value) {
+    "use strict";
     this.Content[position[0]][position[1]] = value;
 };
 
 Board.prototype.getEmptyCell = function () {
+    "use strict";
     var cell = this.getCell();
     while (!this.isCellEmpty(cell)) {
         cell = this.getCell();
@@ -42,18 +51,21 @@ Board.prototype.getEmptyCell = function () {
 };
 
 Board.prototype.fillBoard = function () {
+    "use strict";
     this.LastInserted = this.getEmptyCell();
     this.setCellValue(this.LastInserted, Math.random() > 0.75 ? 4 : 2);
 };
 
 Board.prototype.IsBlocked = function () {
-    for (var x = 0 ; x < 4; x++) {
-        for (var currentYIndex = 0; currentYIndex < 3; currentYIndex++) {
+    "use strict";
+    var x, currentYIndex;
+    for (x = 0; x < 4; x++) {
+        for (currentYIndex = 0; currentYIndex < 3; currentYIndex++) {
             if (this.isCellEmpty([x, currentYIndex]) || this.isCellEmpty([currentYIndex, x])) {
                 return false;
             }
 
-            if ((this.getCellValue([x, currentYIndex]) == this.getCellValue([x, currentYIndex + 1])) || (this.getCellValue([currentYIndex, x]) == this.getCellValue([currentYIndex + 1, x]))) {
+            if ((this.getCellValue([x, currentYIndex]) === this.getCellValue([x, currentYIndex + 1])) || (this.getCellValue([currentYIndex, x]) === this.getCellValue([currentYIndex + 1, x]))) {
                 return false;
             }
         }
@@ -65,283 +77,253 @@ Board.prototype.IsBlocked = function () {
 /**
  * Move object to left to fill any hole but no Merge
  */
-Board.prototype.MoveLeft = function() {
-	
-	var anyChange = false;
-	
-	for(var x = 0 ; x < 4; x++)
-	{
-		for(var currentYIndex = 0; currentYIndex < 3; currentYIndex++)
-		{
-			// check if currentYIndex is pointing to an empty cell.
-			if( this.isCellEmpty([x,currentYIndex]))
-			{
-				// find the next non-empty cell;
-				var nonEmptyYIndex = currentYIndex +1;
-				while( nonEmptyYIndex < 4 && this.getCellValue([x,nonEmptyYIndex]) === '')
-				{
-					nonEmptyYIndex++;	
-				}
-			
-				if (nonEmptyYIndex != 4)
-				{
-					this.setCellValue([x,currentYIndex], this.getCellValue([x,nonEmptyYIndex]));
-					this.clearCellValue([x,nonEmptyYIndex]);
-					
-					anyChange = true;
-				}
-			}
-		}
-	}
-	
-	return anyChange;
+Board.prototype.MoveLeft = function () {
+    "use strict";
+    var anyChange = false, x, currentYIndex, nonEmptyYIndex;
+
+    for (x = 0; x < 4; x++) {
+        for (currentYIndex = 0; currentYIndex < 3; currentYIndex++) {
+            // check if currentYIndex is pointing to an empty cell.
+            if (this.isCellEmpty([x, currentYIndex])) {
+                // find the next non-empty cell;
+                nonEmptyYIndex = currentYIndex + 1;
+                while (nonEmptyYIndex < 4 && this.getCellValue([x, nonEmptyYIndex]) === '') {
+                    nonEmptyYIndex++;
+                }
+
+                if (nonEmptyYIndex !== 4) {
+                    this.setCellValue([x, currentYIndex], this.getCellValue([x, nonEmptyYIndex]));
+                    this.clearCellValue([x, nonEmptyYIndex]);
+
+                    anyChange = true;
+                }
+            }
+        }
+    }
+
+    return anyChange;
 };
 
 /**
  * Merge object on left
  */
-Board.prototype.MergeLeft = function() {
-	for(var x = 0 ; x < 4; x++)
-	{
-		for(var currentYIndex = 0; currentYIndex < 3; currentYIndex++)
-		{
-			// check if next cell is the same.
-			if( !this.isCellEmpty([x,currentYIndex]) && this.getCellValue([x, currentYIndex]) == this.getCellValue([x, currentYIndex +1]))
-			{
-				this.setCellValue([x,currentYIndex], 2 * this.getCellValue([x,currentYIndex]));
-				this.clearCellValue([x, currentYIndex +1]);
-			}
-		}
-	}
+Board.prototype.MergeLeft = function () {
+    "use strict";
+    var x, currentYIndex;
+    for (x = 0; x < 4; x++) {
+        for (currentYIndex = 0; currentYIndex < 3; currentYIndex++) {
+            // check if next cell is the same.
+            if (!this.isCellEmpty([x, currentYIndex]) && this.getCellValue([x, currentYIndex]) === this.getCellValue([x, currentYIndex + 1])) {
+                this.setCellValue([x, currentYIndex], 2 * this.getCellValue([x, currentYIndex]));
+                this.clearCellValue([x, currentYIndex + 1]);
+            }
+        }
+    }
 };
 
 /**
  * Merge object on Up
  */
-Board.prototype.MergeUp = function() {
-	for(var y = 0 ; y < 4; y++)
-	{
-		for(var currentXIndex = 0; currentXIndex < 3; currentXIndex++)
-		{
-			// check if next cell is the same.
-			if( !this.isCellEmpty([currentXIndex, y]) && this.getCellValue([currentXIndex, y]) == this.getCellValue([currentXIndex + 1, y]))
-			{
-				this.setCellValue([currentXIndex, y], 2 * this.getCellValue([currentXIndex, y]));
-				this.clearCellValue([currentXIndex + 1, y]);
-			}
-		}
-	}
+Board.prototype.MergeUp = function () {
+    "use strict";
+    var y, currentXIndex;
+    for (y = 0; y < 4; y++) {
+        for (currentXIndex = 0; currentXIndex < 3; currentXIndex++) {
+            // check if next cell is the same.
+            if (!this.isCellEmpty([currentXIndex, y]) && this.getCellValue([currentXIndex, y]) === this.getCellValue([currentXIndex + 1, y])) {
+                this.setCellValue([currentXIndex, y], 2 * this.getCellValue([currentXIndex, y]));
+                this.clearCellValue([currentXIndex + 1, y]);
+            }
+        }
+    }
 };
 
 /**
  * Merge object Down
  */
-Board.prototype.MergeDown = function() {
-	for(var y = 0 ; y < 4; y++)
-	{
-		for(var currentXIndex = 3; currentXIndex > 0; currentXIndex--)
-		{
-			// check if next cell is the same.
-			if( !this.isCellEmpty([currentXIndex, y]) && this.getCellValue([currentXIndex, y]) == this.getCellValue([currentXIndex - 1, y]))
-			{
-				this.setCellValue([currentXIndex, y], 2 * this.getCellValue([currentXIndex, y]));
-				this.clearCellValue([currentXIndex - 1, y]);
-			}
-		}
-	}
+Board.prototype.MergeDown = function () {
+    "use strict";
+    var y, currentXIndex;
+    for (y = 0; y < 4; y++) {
+        for (currentXIndex = 3; currentXIndex > 0; currentXIndex--) {
+            // check if next cell is the same.
+            if (!this.isCellEmpty([currentXIndex, y]) && this.getCellValue([currentXIndex, y]) === this.getCellValue([currentXIndex - 1, y])) {
+                this.setCellValue([currentXIndex, y], 2 * this.getCellValue([currentXIndex, y]));
+                this.clearCellValue([currentXIndex - 1, y]);
+            }
+        }
+    }
 };
 
 /**
  * Merge object on Right
  */
-Board.prototype.MergeRight = function() {
-	for(var x = 0 ; x < 4; x++)
-	{
-		for(var currentYIndex = 3; currentYIndex > 0; currentYIndex--)
-		{
-			// check if next cell is the same.
-			if( !this.isCellEmpty([x,currentYIndex]) && this.getCellValue([x, currentYIndex]) == this.getCellValue([x, currentYIndex  - 1]))
-			{
-				this.setCellValue([x,currentYIndex], 2 * this.getCellValue([x,currentYIndex]));
-				this.clearCellValue([x, currentYIndex  - 1]);
-			}
-		}
-	}
+Board.prototype.MergeRight = function () {
+    "use strict";
+    var x, currentYIndex;
+    for (x = 0; x < 4; x++) {
+        for (currentYIndex = 3; currentYIndex > 0; currentYIndex--) {
+            // check if next cell is the same.
+            if (!this.isCellEmpty([x, currentYIndex]) && this.getCellValue([x, currentYIndex]) === this.getCellValue([x, currentYIndex - 1])) {
+                this.setCellValue([x, currentYIndex], 2 * this.getCellValue([x, currentYIndex]));
+                this.clearCellValue([x, currentYIndex - 1]);
+            }
+        }
+    }
 };
 
-Board.prototype.DoLeft  = function()
-{
-	this.LastInserted = [-1, -1]
-	var anyChange = this.MoveLeft();
-	this.MergeLeft();
-	anyChange |= this.MoveLeft();
-	
-	if(anyChange)
-	{
-		this.fillBoard();
-	}
-	
-	if(this.IsBlocked())
-	{
-		alert('looossseer');	
-	}
+Board.prototype.DoLeft = function () {
+    "use strict";
+    this.LastInserted = [-1, -1];
+    var anyChange = this.MoveLeft();
+    this.MergeLeft();
+    anyChange = anyChange || this.MoveLeft();
+
+    if (anyChange) {
+        this.fillBoard();
+    }
+
+    if (this.IsBlocked()) {
+        alert('looossseer');
+    }
 };
 
-Board.prototype.DoRight  = function()
-{
-	this.LastInserted = [-1, -1]
-	var anyChange = this.MoveRight();
-	this.MergeRight();
-	anyChange |= this.MoveRight();
-	
-	if(anyChange)
-	{
-		this.fillBoard();
-	}
-	
-	if(this.IsBlocked())
-	{
-		alert('looossseer');	
-	}
+Board.prototype.DoRight = function () {
+    "use strict";
+    this.LastInserted = [-1, -1];
+    var anyChange = this.MoveRight();
+    this.MergeRight();
+    anyChange =  anyChange || this.MoveRight();
+
+    if (anyChange) {
+        this.fillBoard();
+    }
+
+    if (this.IsBlocked()) {
+        alert('looossseer');
+    }
 };
 
-Board.prototype.DoUp  = function()
-{
-	this.LastInserted = [-1, -1]
-	var anyChange = this.MoveUp();
-	this.MergeUp();
-	 anyChange |= this.MoveUp();
-	 
-	if(anyChange)
-	{
-		this.fillBoard();
-	}
-	
-	if(this.IsBlocked())
-	{
-		alert('looossseer');	
-	}
+Board.prototype.DoUp = function () {
+    "use strict";
+    this.LastInserted = [-1, -1];
+    var anyChange = this.MoveUp();
+    this.MergeUp();
+    anyChange = anyChange || this.MoveUp();
+
+    if (anyChange) {
+        this.fillBoard();
+    }
+
+    if (this.IsBlocked()) {
+        alert('looossseer');
+    }
 };
 
-Board.prototype.DoDown  = function()
-{
-	this.LastInserted = [-1, -1]
-	var anyChange = this.MoveDown();
-	this.MergeDown();
-	anyChange |= this.MoveDown();
-	
-	if(anyChange)
-	{
-		this.fillBoard();
-	}
-	
-	if(this.IsBlocked())
-	{
-		alert('looossseer');	
-	}
+Board.prototype.DoDown = function () {
+    "use strict";
+    this.LastInserted = [-1, -1];
+    var anyChange = this.MoveDown();
+    this.MergeDown();
+    anyChange = anyChange || this.MoveDown();
+
+    if (anyChange) {
+        this.fillBoard();
+    }
+
+    if (this.IsBlocked()) {
+        alert('looossseer');
+    }
 };
 
 /**
  * Move object to up to fill any hole but no Merge
  */
-Board.prototype.MoveUp = function() {
-	var anyChange = false;
-	
-	for(var y = 0 ; y < 4; y++)
-	{
-		for(var currentXIndex = 0; currentXIndex < 3; currentXIndex++)
-		{
-			// check if currentYIndex is pointing to an empty cell.
-			if( this.getCellValue([currentXIndex, y]) === '')
-			{
-				// find the next non-empty cell;
-				var nonEmptyXIndex = currentXIndex +1;
-				while( nonEmptyXIndex < 4 && this.getCellValue([nonEmptyXIndex, y]) === '')
-				{
-					nonEmptyXIndex++;	
-				}
-			
-				if (nonEmptyXIndex != 4)
-				{
-					this.setCellValue([currentXIndex, y], this.getCellValue([nonEmptyXIndex, y]));
-					this.clearCellValue([nonEmptyXIndex, y]);
-					
-					anyChange = true;
-				}
-			}
-		}
-	}
-	
-	return anyChange;
+Board.prototype.MoveUp = function () {
+    "use strict";
+
+    var anyChange = false, y, currentXIndex, nonEmptyXIndex;
+
+    for (y = 0; y < 4; y++) {
+        for (currentXIndex = 0; currentXIndex < 3; currentXIndex++) {
+            // check if currentYIndex is pointing to an empty cell.
+            if (this.getCellValue([currentXIndex, y]) === '') {
+                // find the next non-empty cell;
+                nonEmptyXIndex = currentXIndex + 1;
+                while (nonEmptyXIndex < 4 && this.getCellValue([nonEmptyXIndex, y]) === '') {
+                    nonEmptyXIndex++;
+                }
+
+                if (nonEmptyXIndex !== 4) {
+                    this.setCellValue([currentXIndex, y], this.getCellValue([nonEmptyXIndex, y]));
+                    this.clearCellValue([nonEmptyXIndex, y]);
+
+                    anyChange = true;
+                }
+            }
+        }
+    }
+
+    return anyChange;
 };
 
 /**
  * Move object to up to fill any hole but no Merge
  */
-Board.prototype.MoveDown = function() {
-	
-	var anyChange = false;
-	
-	for(var y = 0 ; y < 4; y++)
-	{
-		for(var currentXIndex = 3; currentXIndex > 0; currentXIndex--)
-		{
-			// check if currentYIndex is pointing to an empty cell.
-			if( this.getCellValue([currentXIndex, y]) === '')
-			{
-				// find the next non-empty cell;
-				var nonEmptyXIndex = currentXIndex  -1;
-				while( nonEmptyXIndex >= 0 && this.getCellValue([nonEmptyXIndex, y]) === '')
-				{
-					nonEmptyXIndex--;	
-				}
-			
-				if (nonEmptyXIndex >= 0)
-				{
-					this.setCellValue([currentXIndex, y], this.getCellValue([nonEmptyXIndex, y]));
-					this.clearCellValue([nonEmptyXIndex, y]);
-					
-					anyChange = true;
-				}
-			}
-		}
-	}
-	
-	return anyChange;
+Board.prototype.MoveDown = function () {
+    "use strict";
+    var anyChange = false, y, currentXIndex, nonEmptyXIndex;
+
+    for (y = 0; y < 4; y++) {
+        for (currentXIndex = 3; currentXIndex > 0; currentXIndex--) {
+            // check if currentYIndex is pointing to an empty cell.
+            if (this.getCellValue([currentXIndex, y]) === '') {
+                // find the next non-empty cell;
+                nonEmptyXIndex = currentXIndex - 1;
+                while (nonEmptyXIndex >= 0 && this.getCellValue([nonEmptyXIndex, y]) === '') {
+                    nonEmptyXIndex--;
+                }
+
+                if (nonEmptyXIndex >= 0) {
+                    this.setCellValue([currentXIndex, y], this.getCellValue([nonEmptyXIndex, y]));
+                    this.clearCellValue([nonEmptyXIndex, y]);
+
+                    anyChange = true;
+                }
+            }
+        }
+    }
+
+    return anyChange;
 };
 
 
 /**
  * Move object to right to fill any hole but no Merge
  */
-Board.prototype.MoveRight = function() {
-	
-	var anyChange = false;
-	
-	for(var x = 0 ; x < 4; x++)
-	{
-		for(var currentYIndex = 3; currentYIndex > 0; currentYIndex--)
-		{
-			// check if currentYIndex is pointing to an empty cell.
-			if( this.getCellValue([x,currentYIndex]) === '')
-			{
-				// find the next non-empty cell;
-				var nonEmptyYIndex = currentYIndex  - 1;
-				while( nonEmptyYIndex >= 0 && this.getCellValue([x,nonEmptyYIndex]) === '')
-				{
-					nonEmptyYIndex--;	
-				}
-			
-				if (nonEmptyYIndex >= 0)
-				{
-					this.setCellValue([x,currentYIndex], this.getCellValue([x,nonEmptyYIndex]));
-					this.clearCellValue([x,nonEmptyYIndex]);
-					
-					anyChange = true;
-				}
-			}
-		}
-	}
-	
-	return anyChange;
+Board.prototype.MoveRight = function () {
+    "use strict";
+    var anyChange = false, x, currentYIndex, nonEmptyYIndex;
+
+    for (x = 0; x < 4; x++) {
+        for (currentYIndex = 3; currentYIndex > 0; currentYIndex--) {
+            // check if currentYIndex is pointing to an empty cell.
+            if (this.getCellValue([x, currentYIndex]) === '') {
+                // find the next non-empty cell;
+                nonEmptyYIndex = currentYIndex - 1;
+                while (nonEmptyYIndex >= 0 && this.getCellValue([x, nonEmptyYIndex]) === '') {
+                    nonEmptyYIndex--;
+                }
+
+                if (nonEmptyYIndex >= 0) {
+                    this.setCellValue([x, currentYIndex], this.getCellValue([x, nonEmptyYIndex]));
+                    this.clearCellValue([x, nonEmptyYIndex]);
+
+                    anyChange = true;
+                }
+            }
+        }
+    }
+
+    return anyChange;
 };
--- My2048/Swipe.js
+++ My2048/Swipe.js
@@ -9,55 +9,52 @@ var SwipeDirection = {
     Down: 4
 };
 
-if (Object.freeze)
+if (Object.freeze) {
     Object.freeze(SwipeDirection);
+}
 
+/**
+    Hello documenation
+ */
 function addSwipeListener(el, callback) {
 
-    var touchsurface = el,
-    swipedir,
-    startX,
-    startY,
-    distX,
-    distY,
-    threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
-    startTime,
-    handleswipe = callback || function (swipedir) { }
+    "use strict";
+    var touchsurface = el, swipedir, startX, startY, distX, distY;
+    var threshold = 150; //required min distance traveled to be considered swipe
+    var restraint = 100; // maximum distance allowed at the same time in perpendicular direction
+    var allowedTime = 300; // maximum time allowed to travel that distance
+    var elapsedTime, startTime;
+    var handleswipe = callback || function (dummy) { };
 
     touchsurface.addEventListener('touchstart', function (e) {
-        var touchobj = e.changedTouches[0]
-        swipedir = 'none'
-        dist = 0
-        startX = touchobj.pageX
-        startY = touchobj.pageY
-        startTime = new Date().getTime() // record time when finger first makes contact with surface
-        e.preventDefault()
+        var touchobj = e.changedTouches[0];
+        swipedir = 'none';
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime(); // record time when finger first makes contact with surface
+        e.preventDefault();
 
-    }, false)
+    }, false);
 
     touchsurface.addEventListener('touchmove', function (e) {
-        e.preventDefault() // prevent scrolling when inside DIV
-    }, false)
+        e.preventDefault();// prevent scrolling when inside DIV
+    }, false);
 
     touchsurface.addEventListener('touchend', function (e) {
-        var touchobj = e.changedTouches[0]
-        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
-        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
-        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        var touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime; // get time elapsed
         if (elapsedTime <= allowedTime) { // first condition for awipe met
             if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0) ? SwipeDirection.Left : SwipeDirection.Right // if dist traveled is negative, it indicates left swipe
-            }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
-                swipedir = (distY < 0) ? SwipeDirection.Up : SwipeDirection.Down // if dist traveled is negative, it indicates up swipe
+                swipedir = (distX < 0) ? SwipeDirection.Left : SwipeDirection.Right; // if dist traveled is negative, it indicates left swipe
+            } else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+                swipedir = (distY < 0) ? SwipeDirection.Up : SwipeDirection.Down; // if dist traveled is negative, it indicates up swipe
             }
         }
-        handleswipe(swipedir)
-        e.preventDefault()
-    }, false)
+        handleswipe(swipedir);
+        e.preventDefault();
+    }, false);
 }
 
 //USAGE:
