ab4118df746a66806d79265685cf014bc73768b4
Frederic Forjan <frederic.forjan@schneider-electric.com>
2014-03-21 17:33:26 +1100

    add simple game type

---
+++ My2048/app/js/app.js
@@ -0,0 +1,3 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+
+window.Manager2048 = { Models: {}, Collections: {}, Views: {} };
---
+++ My2048/app/js/models/gametype.js
@@ -0,0 +1,12 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+
+Manager2048.Models.GameType = Backbone.Model.extend({
+    defaults: {
+        name: null,
+        screenshot: null
+    },
+    initialize: function () {
+        "use strict";
+        this.set('screenshot', _.random(1, 15) + '.jpg');
+    }
+});
