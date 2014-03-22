140cbfc7146119553e938f0800872652da75a972
Frederic Forjan <fforjan@free.fr>
2014-03-22 11:34:01 +1100

    update application manager

--- My2048/app/js/app.js
+++ My2048/app/js/app.js
@@ -1,3 +1,10 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 
-window.Manager2048 = { Models: {}, Collections: {}, Views: {} };
+window.Manager2048 = { 
+    Models: {}, 
+    Collections: {}, 
+    Views: {},
+    start: function() {
+        window.alert('2048 Manager started!');
+    }};
+
