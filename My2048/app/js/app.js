a44f67206383a61286348a64ee9f7105524778ad
Frederic Forjan <fforjan@free.fr>
2014-03-22 11:37:05 +1100

    jslint modification

--- My2048/app/js/app.js
+++ My2048/app/js/app.js
@@ -1,10 +1,12 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
 
-window.Manager2048 = { 
-    Models: {}, 
-    Collections: {}, 
+window.Manager2048 = {
+    Models: {},
+    Collections: {},
     Views: {},
-    start: function() {
+    start: function () {
+        "use strict";
         window.alert('2048 Manager started!');
-    }};
+    }
+};
 
