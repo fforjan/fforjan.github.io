8c455cc98becc9c92a986d20bc262c7eeee5c081
Frederic Forjan <frederic.forjan@schneider-electric.com>
2014-03-24 18:18:40 +1100

    render with canvas

--- My2048/app/js/views/game-canvas.js
+++ My2048/app/js/views/game-canvas.js
@@ -13,30 +13,63 @@ Manager2048.Views.GameCanvas = Backbone.View.extend({
         return this;
     },
     
+    createCell : function () {
+        "use strict";
+        var rect = new Kinetic.Rect({
+            x: 2.5,
+            y: 2.5,
+            width: 95,
+            height: 95,
+            fill: 'lightgrey',
+            cornerRadius: 10,
+        });
+                
+        var text = new Kinetic.Text({
+            x: 0,
+            y: 37,
+            width: 100,
+            height: 100,
+            align: 'center',
+            text: '0',
+            fontSize: 30,            
+            fill: 'green'
+        });
+      
+        var group = new Kinetic.Group();
+        
+        group.add(rect);
+        group.add(text);
+        
+        return group;
+    },
+    
+    createBoardUI: function (layer) {
+        "use strict";
+        
+        var x, y;
+        for (x = 0; x < 4; x = x + 1) {
+            for (y = 0; y < 4; y = y + 1) {
+                var group = this.createCell();
+                group.setPosition({x: x * 100, y  : y * 100});
+                
+                layer.add(group);
+            }
+        }
+    },
+    
     rendered: function () {
         "use strict";
         
         this.stage = new Kinetic.Stage({
             container: 'kinetic',
-            width: 578,
-            height: 200
+            width: 600,
+            height: 600
         });
 
         var layer = new Kinetic.Layer();
-
-        var rect = new Kinetic.Rect({
-            x: 239,
-            y: 75,
-            width: 100,
-            height: 50,
-            fill: 'green',
-            stroke: 'black',
-            strokeWidth: 4
-        });
-
-        // add the shape to the layer
-        layer.add(rect);
-
+      
+        this.createBoardUI(layer);
+                    
         // add the layer to the stage
         this.stage.add(layer);
                 
