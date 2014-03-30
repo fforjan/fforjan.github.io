40b49410bc7fe20c9dd6a0e3ac8e1b9d080e1a26
Frederic Forjan <fforjan@free.fr>
2014-03-30 20:43:35 +1100

    use angularjs for controlling

---
+++ French-A-Faire/app/js/AFaireApplication.js
@@ -0,0 +1,48 @@
+var AFaireApplication = angular.module('AFaireApplication', []);
+
+AFaireApplication.controller('AFaireController', function ($scope) {
+    "use strict";
+    $scope.todos = [
+        {
+            'name': 'Todo #1',
+            'text': 'Content #1'
+        },
+        {
+            'name': 'Todo #2',
+            'text': 'Content #'
+        },
+        {
+            'name': 'Todo #3',
+            'text': 'Content #3'
+        },
+        {
+            'name': 'Todo #4',
+            'text': 'Content #4'
+        },
+        {
+            'name': 'Todo #5',
+            'text': 'Content #5'
+        },
+        {
+            'name': 'Todo #6',
+            'text': 'Content #6'
+        },
+        {
+            'name': 'Todo #7',
+            'text': 'Content #7'
+        },
+        {
+            'name': 'Todo #8',
+            'text': 'Content #8'
+        },
+        {
+            'name': 'Todo #8',
+            'text': 'Content #8'
+        },
+        {
+            'name': 'Todo #8',
+            'text': 'Content #8'
+        }
+        
+    ];
+});
