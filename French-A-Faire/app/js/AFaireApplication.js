ef66546bd8abe373172b25336c6ac6b353231b70
Frederic Forjan <fforjan@free.fr>
2014-03-30 21:09:17 +1100

    use local storage

--- French-A-Faire/app/js/AFaireApplication.js
+++ French-A-Faire/app/js/AFaireApplication.js
@@ -1,48 +1,58 @@
-var AFaireApplication = angular.module('AFaireApplication', []);
+var AFaireApplication = angular.module('AFaireApplication', ['LocalStorageModule']);
 
-AFaireApplication.controller('AFaireController', function ($scope) {
+AFaireApplication.config(['localStorageServiceProvider', function (localStorageServiceProvider) {
     "use strict";
-    $scope.todos = [
-        {
-            'name': 'Todo #1',
-            'text': 'Content #1'
-        },
-        {
-            'name': 'Todo #2',
-            'text': 'Content #'
-        },
-        {
-            'name': 'Todo #3',
-            'text': 'Content #3'
-        },
-        {
-            'name': 'Todo #4',
-            'text': 'Content #4'
-        },
-        {
-            'name': 'Todo #5',
-            'text': 'Content #5'
-        },
-        {
-            'name': 'Todo #6',
-            'text': 'Content #6'
-        },
-        {
-            'name': 'Todo #7',
-            'text': 'Content #7'
-        },
-        {
-            'name': 'Todo #8',
-            'text': 'Content #8'
-        },
-        {
-            'name': 'Todo #8',
-            'text': 'Content #8'
-        },
-        {
-            'name': 'Todo #8',
-            'text': 'Content #8'
-        }
-        
-    ];
-});
+    localStorageServiceProvider.setPrefix('AFaireApplication');
+}]);
+
+AFaireApplication.controller('AFaireController', [ '$scope', 'localStorageService', function ($scope, localStorageService) {
+    "use strict";
+
+    var todos = localStorageService.get('todos');
+    if (todos === null) {
+        todos = [
+            {
+                'name': 'Todo #1',
+                'text': 'Content #1'
+            },
+            {
+                'name': 'Todo #2',
+                'text': 'Content #'
+            },
+            {
+                'name': 'Todo #3',
+                'text': 'Content #3'
+            },
+            {
+                'name': 'Todo #4',
+                'text': 'Content #4'
+            },
+            {
+                'name': 'Todo #5',
+                'text': 'Content #5'
+            },
+            {
+                'name': 'Todo #6',
+                'text': 'Content #6'
+            },
+            {
+                'name': 'Todo #7',
+                'text': 'Content #7'
+            },
+            {
+                'name': 'Todo #8',
+                'text': 'Content #8'
+            },
+            {
+                'name': 'Todo #8',
+                'text': 'Content #8'
+            },
+            {
+                'name': 'Todo #8',
+                'text': 'Content #8'
+            }];
+        localStorageService.add('todos',  todos);
+    } 
+
+    $scope.todos = todos;
+}]);
